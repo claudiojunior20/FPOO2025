9eac38fe4b056042e59780736b48697c43316386
Claudio junior <jukusma20@gmail.com>
2025-04-12 01:49:23 -0300

    projeto adicionado JUnit

--- semaphore/src/test/java/semaphore/light/e27/E27LightBulbTest.java
+++ semaphore/src/test/java/semaphore/light/e27/E27LightBulbTest.java
@@ -1,8 +1,11 @@
 package semaphore.light.e27;
 
-import static org.junit.Assert.*;
 
-import org.junit.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 import semaphore.light.Light;
 
