5e5cd7297207c79f53b451b2562b5ad3f3dfebb1
Claudio <jukusma20@gmail.co>
2025-05-11 23:51:28 -0300

    trabalho

--- semaphore/src/main/java/semaphore/trafficLight/control/SimpleTrafficLightControl.java
+++ semaphore/src/main/java/semaphore/trafficLight/control/SimpleTrafficLightControl.java
@@ -1,14 +1,17 @@
 package semaphore.trafficLight.control;
 
+
 import semaphore.trafficLight.TrafficLight;
 import java.util.Timer;
 import java.util.TimerTask;
+
 
 
 public class SimpleTrafficLightControl implements TrafficLightControl {
   
 	private  final TrafficLight trafficLight;
 	private State state = State.OFF;
+	  private Timer alertTimer;
 	
 	public SimpleTrafficLightControl(TrafficLight trafficLight) {
 		this.trafficLight=trafficLight;
@@ -16,11 +19,36 @@ public class SimpleTrafficLightControl implements TrafficLightControl {
 
 	@Override
 	public void turnAlert() {
-		
-	 state = State.ALERT;
+	state = State.ALERT;
+	if (alertTimer != null) {
+        alertTimer.cancel();
+    }
+    alertTimer = new Timer();
+    alertTimer.scheduleAtFixedRate(new TimerTask() {
+        @Override
+        public void run() {
+            
+            if (trafficLight.spotYellow().isOn()) {
+                trafficLight.spotYellow().turnOff();
+            } else {
+                trafficLight.spotYellow().turnOn();
+            }
+            
+            trafficLight.spotRed().turnOff();
+            trafficLight.spotGreen().turnOff();
+        }
+    }, 0, 2000); 
+}
+	
+	
+	
+		 
+		 
 	 
 	
-	}
+	 
+	
+	
 
 	@Override
 	public void turnGreen() {
